65d6f1096ad4a60fa76a2fdd62cbd73fc5601547
AGodwin15 <agodwin@conncoll.edu>
2016-12-05 15:15:47 -0500

    Update BST.java

--- BST.java
+++ BST.java
@@ -1,4 +1,4 @@
-/*Nate Brown and Edited by Andrew Godwin 
+/*Nate Brown
 October 31
 Program 4: this program is a binary search tree using linked nodes that should 
 test each of the BST functions
@@ -26,6 +26,7 @@ public class BST {
 			return searchR(t, key);
 		}
 	}
+	//Edited by Andrew Godwin 
 //search function given the tree and the key to return the student record
 	private Node searchR(Node t2, int key) {
 		if (t2 == null) {
