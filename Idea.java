4c79ab22c093779943ec2468fd6801865163aea1
Nathan Alexander Brown <nbrown2@conncoll.edu>
2016-12-07 15:56:46 -0500

    Update Idea.java

--- Idea.java
+++ Idea.java
@@ -1,25 +1,29 @@
 /*
+Nate Brown
 This is essentially a program that enlists each idea of
 a student into a node within the student node itself
 12/5/16
 */
-
+//Idea class
 public class Idea{ 
+//instance variables for the idea class
 	private int ideaNumber;
 	private int studentSSN;
 	private String description;
 	private int ideaRating;
 
-	  public Idea(int ideaNum, int SSN, String descr, int ideaRate) {
+//establishing the instance variables and what they aree going to be called in the future for future programs
+	public Idea(int ideaNum, int SSN, String descr, int ideaRate) {
 		ideaNumber = ideaNum;
 		studentSSN = SSN;
   		description = descr;
 	  	ideaRating = ideaRate;
-	  }
-
+	}
+//sets the idea num ect...
   	public void setIdeaNum(int ideaNum) {
 	  	ideaNumber = ideaNum;
   	}
+//gets the idea num ect...
 	  public int getIdeaNum() {
 		  return ideaNumber;
   	}
