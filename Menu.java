e0020b6bb7c54e15b280d8ecae56837f979aa3f0
Nathan Alexander Brown <nbrown2@conncoll.edu>
2016-12-11 14:42:44 -0500

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -148,6 +148,40 @@ System.out.println("Hello, Welcome to our Idea Database!");
 	}
 
 	finally {
-		out.close();
+		//Serialize, put this right before quitting the program
+		try {
+			FileOutputStream fileOut = new FileOutputStream("output.txt");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(BST);
+			out.writeObject(QueueA);
+			out.writeObject(Heap);
+			out.close();
+			fileOut.close();
+			System.out.println("Serialized object successfully in output.txt");
+			} 
+       		   catch(IOException i) {
+			  i.printStackTrace();
+			}
+
+//De-serialize, put this at the beginning when you want to load something pre-saved in,
+  //      note: it might error out if the text file doesn't exsist/ was wiped from RAM
+
+		try {
+			FileInputStream fileIn = new FileInputStream("output.txt");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			BST = (StudentBST) in.readObject();
+			QueueA = (IdeaQueue) in.readObject();
+			Heap = (IdeaHeap) in.readObject();
+			in.close();
+			fileIn.close();
+			} 
+			catch(IOException i) {
+			i.printStackTrace();
+			} 
+			catch(ClassNotFoundException j) {
+			j.printStackTrace();
+			}
+
+//Note: for these^, BST, QueueA, etc are new objects we are assiging the stuff from the text fileout.close();
 	}
 }}}
