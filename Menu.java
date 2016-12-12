efd8a779139c767b51780b317385314a8b42c31d
Nathan Alexander Brown <nbrown2@conncoll.edu>
2016-12-11 22:46:44 -0500

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 import java.io.IOException;
 import java.lang.NumberFormatException;
 
-public class Menu implements{ //java.io.Serializable{
-	BST bst = new BST();
+public class Menu /*implements*/{ //java.io.Serializable{
 	public static void main(String[] args) {
+	BST bst = new BST();
+	QueueA queue = new QueueA();
+	
 		
 System.out.println("Hello, Welcome to our Idea Database!");
 /*		System.out.println("Would you like to");
@@ -33,7 +35,8 @@ System.out.println("Hello, Welcome to our Idea Database!");
 // Note: for these^, BST, QueueA, etc are new objects we are assiging the stuff from the text fileout.close();
 			}}
 */
-	while (input != "E" || input !=  "e"){	// while loop
+	String input = "t";
+	while (!input.equals("E") || !input.equals("e")){	// while loop
 		try {		
 		Scanner in = new Scanner(System.in);
 		System.out.println("Choose from one of the following options to proceed:");
@@ -45,134 +48,184 @@ System.out.println("Hello, Welcome to our Idea Database!");
 		System.out.println("(E) Exit the program");
 		System.out.println();
 		System.out.println("What would you like to do? (Ex:'A'): "); // user prompt
-		String input = in.next();
-		if (input == "A" || input == "a") {	
-			bst.createStudentNode()
-			while (input5 != "B" || input5 != "b"){
+		input = in.next();
+		if (input.equals("A") || input.equals("a")) {	
+			bst.createStudentNode();
+			String input5 = "t";
+			while (!input5.equals("B") || !input5.equals("b")){
 				try{
 					System.out.println("Would you like to enter an Idea? (Ex. A) ");
 					System.out.println("(A) Yes");
 					System.out.println("(B) No");
-					String input5 = in.next();
-					if (input5 == "A" || input5 ==  "a"){
+					input5 = in.next();
+					if (input5.equals("A") || input5.equals("a")){
 						try{
 							System.out.println("Enter 9-digit SSN: ");
 							String input6 = in.next();
-							int input2 = Integer.parseInt(input6)
-							if (input2 > 99999999 && input2 < 1000000000){
-								bst.addIdeaStudent(input2); }}
-						catch(NumberFormatException e){
+							int input2 = Integer.parseInt(input6);
+							if (input2 < 99999999 || input2 > 1000000000){
+								System.out.println("That is an invalid SSN, please try again");
+								continue;
+							}
+							else {
+								queue.addIdeaStudent(input2); 
+							}
+						}
+						catch(IndexOutOfBoundsException e){
 							System.out.println();
-							System.out.println(e + "is an invalid SSN, please try again");}}}
-				catch(IOException e) {
+							System.out.println(e + "is an invalid SSN, please try again");
+						}
+					}
+					else if (input5.equals("B") || input5.equals("b")){
+						break;
+				}
+			}
+				catch(Exception e) {
 					System.out.println("Input Error: " + e);
 					System.out.println("That was not a valid entry, please try again.");
 					System.out.println();}}
 		}	
-		else if (input == "B" || input == "b") {	
+		else if (input.equals("B") || input.equals("b")) {	
 			try {
-			Scanner in = new Scanner(System.in);
-			System.out.println();	
-			System.out.println("(A) Search using SSN");
-			System.out.println("(B) Search using Student Number");
-			System.out.println("Which would you like to do (Ex:'A'): ");
-			System.out.println();
-			String input7 = in.next();
-			if (input7 == "A" || input7 == "a") { // Search using SSN
-				try {
-				System.out.println("Enter 9-digit SSN: ");
+				//Scanner in = new Scanner(System.in);
+				System.out.println();	
+				System.out.println("(A) Search using SSN");
+				System.out.println("(B) Search using Student Number");
+				System.out.println("Which would you like to do (Ex:'A'): ");
+				System.out.println();
 				String input7 = in.next();
-				int input3 = Integer.parseInt(input7)
-				if (input3 > 99999999 && input3 < 1000000000){
-					bst.SearchRecord(input3); 
-					System.out.println("Would you like to:");
-					System.out.println("(A) Delete the Student Record");
-					System.out.println("(B) Edit the Last Name or Login Name of the record");
-					System.out.println("Which would you like to do (Ex:'A'): ");
-					String input11 = in.next();
-					try{
-						if (input11 == "A" || input11 == "a"){
-							Node a = searchRecord(input3);
-							bst.delete(a);
-							System.out.println("Student Record has been deleted");}
-						else if(input11 == "B" || input11 == "b"){
+			if (input7.equals("A") || input7.equals("a")) { // Search using SSN
+				try {
+					System.out.println("Enter 9-digit SSN: ");
+					input7 = in.next();
+					int input3 = Integer.parseInt(input7);
+					if (input3 > 99999999 && input3 < 1000000000){
+						StudentNode u = bst.searchRecord(input3); 
+							if (u != null){
+							System.out.println("Student Found! Here is their information:");
+							System.out.println("Email: " + u.getEmail()); 
+						    System.out.println("SSN: " + u.getSocial());
+						    System.out.println("Student Number: " + u.getStunum());
+						    System.out.println("Last Name: " + u.getLast()); 
+						    System.out.println("Average Idea Rating: " + u.getAvgRating());
+							}
+							else if (u == null) { 
+							System.out.println("Sorry that student record was not found!");
+							continue;
+						}	
 							System.out.println("Would you like to:");
-							System.out.println("(A) Change Last Name");
-							System.out.println("(B) Change Login Name");
-							System.out.println("(C) Change Both");
-							String input12 = in.next();
+							System.out.println("(A) Delete the Student Record");
+							System.out.println("(B) Edit the Last Name or Login Name of the record");
+							System.out.println("Which would you like to do (Ex:'A'): ");
+							String input11 = in.next();
+						
 							try{
-								if (input12 == "A" || input12 == "a"){
-									Node a = searchRecord(input3);
-									System.out.println("Enter new Last Name: ");
-									String input13 = in.next();
-									a.setLast(input13);}
-								else if (input12 == "B" || input12 == "b"){
-									Node a = searchRecord(input3);
-									System.out.println("Enter new Login Name: ");
-									String input14 = in.next();
-									a.setEmail(input14);}
-								else if (input12 == "C" || input12 == "c"){
-									Node a = searchRecord(input3);
-									System.out.println("Enter new Last Name: ");
-									String input13 = in.next();
-									a.setLast(input13);
-									System.out.println("Enter new Login Name: ");
-									String input14 = in.next();
-									a.setEmail(input14);}}
-							catch(IOException e) {
+								if (input11.equals("A") || input11.equals("a")){
+									StudentNode a = bst.searchRecord(input3);
+									bst.delete(a);
+									System.out.println("Student Record has been deleted");
+								}
+								else if(input11.equals("B") || input11.equals("b")){
+									System.out.println("Would you like to:");
+									System.out.println("(A) Change Last Name");
+									System.out.println("(B) Change Login Name");
+									System.out.println("(C) Change Both");
+									String input12 = in.next();
+							
+								try{
+									if (input12.equals("A") || input12.equals("a")){
+										StudentNode a = bst.searchRecord(input3);
+										System.out.println("Enter new Last Name: ");
+										String input13 = in.next();
+										a.setLast(input13);
+									}
+									else if (input12.equals("B") || input12.equals("b")){
+										StudentNode a = bst.searchRecord(input3);
+										System.out.println("Enter new Login Name: ");
+										String input14 = in.next();
+										a.setEmail(input14);
+									}
+									else if (input12.equals("C") || input12.equals("c")){
+										StudentNode a = bst.searchRecord(input3);
+										System.out.println("Enter new Last Name: ");
+										String input13 = in.next();
+										a.setLast(input13);
+										System.out.println("Enter new Login Name: ");
+										String input14 = in.next();
+										a.setEmail(input14);
+									}
+								}
+								catch(Exception e) {
+									System.out.println("Input Error: " + e);
+									System.out.println("That was not a valid entry, please try again.");
+									System.out.println();
+								}
+								}
+							}
+							catch(Exception e) {
 								System.out.println("Input Error: " + e);
 								System.out.println("That was not a valid entry, please try again.");
-								System.out.println();}
-						}
-					catch(IOException e) {
-						System.out.println("Input Error: " + e);
-						System.out.println("That was not a valid entry, please try again.");
-						System.out.println();}
-				}}
+								System.out.println();
+							}		
+					}
+				}
 				catch(NumberFormatException e){
 					System.out.println();
 					System.out.println(e + "is an invalid SSN, please try again");
-			}}
-			else if (input == "B" || input == "b") {	// Search using student number
+				}
+			}
+
+			else if (input.equals("B") || input.equals("b")) {	// Search using student number
 				try {
-				System.out.println("Enter 4-digit Student Number: ");
-				String input8 = in.next();
-				int input4 = Integer.parseInt(input8)
-				if (input4 > 999 && input4 < 10000){
-					bst.SearchEmail(input4); }}
+					System.out.println("Enter 4-digit Student Number: ");
+					String input8 = in.next();
+					int input4 = Integer.parseInt(input8);
+					if (input4 > 999 && input4 < 10000){
+						bst.searchEmail(input4); 
+					}
+				}
 				catch(NumberFormatException e){
 					System.out.println();
 					System.out.println(e + "is an invalid Student Number, please try again");
-			}}}
-			catch(IOException e) {
+				}
+			}
+			}
+			catch(Exception e) {
 				System.out.println("Input Error: " + e);
 				System.out.println("That was not a valid entry, please try again.");
-				System.out.println();}
+				System.out.println();
 			}
+		}
 			
-		else if (input == "C" || input == "c") {
+		else if (input.equals("C") || input.equals("c")) {
 			bst.printTree();
 		}	
-		else if (input == "D" || input == "d") {
+		else if (input.equals("D") || input.equals("d")) {
 			try {
 				System.out.println("Input the SSN of the student you would like to add the idea to: ");
 				String input9 = in.next();
-				int input10 = Integer.parseInt(input9)
+				int input10 = Integer.parseInt(input9);
 				if (input10 > 99999999 && input10 < 1000000000){
-					bst.addIdeaStudent(input10); }}
+					queue.addIdeaStudent(input10); 
+				}
+			}
 				catch(NumberFormatException e){
 					System.out.println();
-					System.out.println(e + "is an invalid SSN, please try again");}
+					System.out.println(e + "is an invalid SSN, please try again");
+				}
+		}
+		else if (input.equals("E") || input.equals("e")) {
+			System.out.println("Thanks for using our program, have a lovely day");
+			System.exit(0);
 		}	
 	}
-		catch(IOException e) {
+		catch(Exception e) {
 			System.out.println("Input Error: " + e);
 			System.out.println("That was not a valid entry, please try again.");
 			System.out.println();
 	}
 
-	finally {
+	//finally {
 		//Serialize, put this right before quitting the program
 		/*try {
 			FileOutputStream fileOut = new FileOutputStream("output.txt");
@@ -187,13 +240,15 @@ System.out.println("Hello, Welcome to our Idea Database!");
        		   catch(IOException i) {
 			  i.printStackTrace();
 			}
-
 //De-serialize, put this at the beginning when you want to load something pre-saved in,
   //      note: it might error out if the text file doesn't exsist/ was wiped from RAM
-
 		
-
 //Note: for these^, BST, QueueA, etc are new objects we are assiging the stuff from the text file
 */
-out.close();	
-}}}
+		//out.close();
+	//}	
+	}
+	System.exit(0);
+}
+
+}
