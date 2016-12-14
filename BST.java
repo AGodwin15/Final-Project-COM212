addb2e8c51cbfedba3c6ecfeba9dc786dd91e734
Nathan Alexander Brown <nbrown2@conncoll.edu>
2016-12-13 20:58:09 -0500

    Update BST.java

--- BST.java
+++ BST.java
@@ -10,11 +10,13 @@ import java.util.Scanner;
 public class BST implements java.io.Serializable{
 	//public QueueA QueueA.java
 	private StudentNode t;
-	BST2 bst2 = new BST2();
+	private StudentNode s;
+	//BST2 bst2 = new BST2();
 
 //create an empty tree	
 	public BST() {
 		t = null;
+		s = null;
 	}
 //checks if tree is empty
 	public boolean isEmptyTree() {
@@ -22,6 +24,38 @@ public class BST implements java.io.Serializable{
 	}
 //searches for the desired node through the given key
 	//this search method uses the SSN as the key to get the full student record 
+//FOR SSN
+	public StudentNode searchRecordSSN(int key) {
+//checks first if tree is empty, returning null
+
+		if (s == null) {
+			return null;
+		}
+//otherwise, continue with the search given the key 
+		else {
+			return searchRrecordSSN(s, key);
+		}
+	}
+	//Edited by Andrew Godwin 
+//search function given the tree and the key to return the student record
+	//this function uses the SSN to get the full student record 
+//FOR SSN
+	private StudentNode searchRrecordSSN(StudentNode s2, int key) {
+		if (s2 == null) {
+			return null;
+		}
+		else if (key == s2.getSocial()) {
+			//return student record;
+			return s2;
+		}
+		else if (key < s2.getSocial()) {
+			return searchRrecordSSN(s2.getLeft2(), key); 
+		}	
+		else {
+			return searchRrecordSSN(s2.getRight2(), key);
+		}
+	}
+//FOR STU NUM
 	public StudentNode searchRecord(int key) {
 //checks first if tree is empty, returning null
 		if (t == null) {
@@ -39,11 +73,11 @@ public class BST implements java.io.Serializable{
 		if (t2 == null) {
 			return null;
 		}
-		else if (key == t2.getSocial()) {
+		else if (key == t2.getStunum()) {
 			//return student record;
 			return t2;
 		}
-		else if (key < t2.getSocial()) {
+		else if (key < t2.getStunum()) {
 			return searchRrecord(t2.getLeft(), key); 
 		}	
 		else {
@@ -87,38 +121,41 @@ public class BST implements java.io.Serializable{
 		}
 			
 	}
-	//Edited by Andrew 
-	public void createStudentNode(){
-		Scanner in = new Scanner(System.in);
-		System.out.println("Enter last name: ");
-		String lastName = in.next();
-		System.out.println("Enter login name: ");
-		String loginName = in.next();
-		System.out.println("Enter the SSN: ");
-		int socialSecurityNew = in.nextInt();
-		try {
-			if (99999999 < socialSecurityNew && socialSecurityNew < 1000000000) {
-				socialSecurityNew = (socialSecurityNew%10000);
+
+	//Edited by Nate
+	//FOR SSN 
+public void insertSSN(StudentNode p) {
+		if (s == null) {
+			s = p;
+		}
+//otherwise, runs the function insert2	
+		else {
+			insertSSN2(s,p);
+		}
+	}
+	
+//inserts the desired node into the tree by checking where it
+//should be in the tree based on the key and whats in the tree
+	private void insertSSN2(StudentNode s3, StudentNode p) {
+		if (p.getSocial() < s3.getSocial()) {
+			if (s3.getLeft2() == null) {
+				s3.setLeft2(p);
+			}
+			else {
+				insertSSN2(s3.getLeft2(), p);
 			}
 		}
-		catch(IndexOutOfBoundsException e){
-			System.out.println("Sorry but " + e + " is not a valid social security number!");
-			System.out.println("Please try again!");
-			System.out.println("Enter the SSN: ");
-			socialSecurityNew = in.nextInt();
+		else {
+			if (s3.getRight2() == null) {
+				s3.setRight2(p);
+			}
+			else {
+				insertSSN2(s3.getRight2(), p);
+			}
 		}
-			
-		System.out.println("Enter student number: ");
-		int studentNumber = in.nextInt();
-		QueueA queue = new QueueA();
-		StudentNode student = new StudentNode(lastName, loginName, socialSecurityNew, studentNumber);
-		in.nextLine();
-		insert(student);
-		bst2.insert(student);
-
-	}	
+	}
 		
-	
+//FOR STU NUM
 //checks if there is no root, inserting the new node as the root
 	public void insert(StudentNode p) {
 		if (t == null) {
@@ -148,6 +185,21 @@ public class BST implements java.io.Serializable{
 				insert2(t3.getRight(), p);
 			}
 		}
+	}	
+	public void traverseSSN() {
+		if (s != null) {
+			traverseRSSN(s.getLeft2());
+			System.out.print(s.getSocial() + " ");
+			traverseRSSN(s.getRight2());
+	}
+}
+//gets the left and right values of the node, and prints out the key
+	private void traverseRSSN(StudentNode s4) {
+		if (s4!=null) {
+			traverseRSSN(s4.getLeft2());
+			System.out.print(s4.getSocial() + " ");
+			traverseRSSN(s4.getRight());
+		}
 	}
 //gets the left and right values of the node, and prints out the key
 	public void traverse() {
@@ -165,6 +217,83 @@ public class BST implements java.io.Serializable{
 			traverseR(t4.getRight());
 		}
 	}
+
+//FOR SSN
+public void deleteSSN(StudentNode p) {
+//case if root == desired delete location
+		if (p == s) {
+			if (s.getRight2() == null && s.getLeft2() == null) {
+				 s = null;
+			}
+			else if (s.getRight2() != null && s.getLeft2() != null) {
+				StudentNode g = successorSSN(s);
+				g.setLeft2(s.getLeft2());
+				if (s.getRight2() == g) {
+					g.setRight2(null);
+				}
+				else {
+	
+					if (g.getRight() != null) {
+						StudentNode parent = findParentSSN(g);
+						parent.setLeft2(g.getRight());
+					}
+					g.setRight2(s.getRight2());
+				}
+				s = g; 
+				
+			}
+			else if (s.getRight2() == null || s.getLeft2() == null) {
+				if (s.getLeft2() == null) {
+					s = s.getRight2();
+				}
+				else {
+					s = s.getLeft2();
+				}
+			}
+		}
+//case if there is a single child
+		else if (p.getRight2() == null || p.getLeft2() == null) {
+			StudentNode parent = findParentSSN(p);
+	
+				if (isLeft(parent, p)) {
+					parent.setLeft2(p.getLeft2());
+					p.setLeft2(null);
+				}
+				else {
+					parent.setRight2(p.getRight2());
+					p.setRight2(null);
+				}
+			
+		}
+//case if there are no children
+		else if (p.getRight2() == null && p.getLeft2() == null) {
+			StudentNode parent = findParentSSN(p);
+			if (isLeft(parent, p)) {
+				parent.setLeft2(null);
+			}
+			else {
+				parent.setRight2(null);
+			}
+		}
+//case if there are two children
+		else if (p.getRight2() != null && p.getLeft2() != null) {
+			StudentNode parent = findParentSSN(p);
+			StudentNode l = successorSSN(p);
+			if (isLeft(parent, p)) {
+				parent.setLeft2(l);
+			}
+			else {
+				parent.setRight2(l);
+			}
+			
+			l.setLeft2(p.getLeft2());
+			if (p.getRight2() != l) {
+				l.setRight2(p.getRight2());
+			}
+		}
+	}
+
+//FOR STU NUM
 //delete function for deleting the desired node 
 	public void delete(StudentNode p) {
 //case if root == desired delete location
@@ -246,7 +375,28 @@ public class BST implements java.io.Serializable{
 			a = a.getLeft();
 		}
 		return a;
-		}	
+		}
+//FOR SSN
+	public StudentNode successorSSN(StudentNode d) {
+		StudentNode a = d.getRight2();
+		while (a.getLeft2() != null) {
+			a = a.getLeft2();
+		}
+		return a;
+		}
+//FOR SSN
+	public StudentNode findParentSSN(StudentNode y) {
+		StudentNode temp = s;
+		while (temp.getRight2() != y && temp.getLeft2() != y) {
+			if (temp.getSocial() > y.getSocial()) {
+				temp = temp.getLeft2();
+			}
+			else {
+				temp = temp.getRight2();
+			}
+		}
+		return temp;
+	}
 //finds the parent of the desired node
 	public StudentNode findParent(StudentNode y) {
 		StudentNode temp = t;
@@ -259,6 +409,16 @@ public class BST implements java.io.Serializable{
 			}
 		}
 		return temp;
+	}
+//checks if node h is left of node g
+//FOR SSN
+	public boolean isLeftSSN(StudentNode g, StudentNode h) {
+		if (g.getLeft2() == h) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 //checks if node h is left of node g
 	public boolean isLeft(StudentNode g, StudentNode h) {
@@ -275,32 +435,53 @@ public class BST implements java.io.Serializable{
 		printTree2(t);
 		System.out.println();
     }
+   
+
+
 //prints the new tree's keys based on positions
 	//Andrew edited 
     private void printTree2(StudentNode tree) {
 	if (tree != null) {
-		System.out.print(tree.getStunum() + " ");
-		System.out.print(tree.getSocial() + " ");
-		System.out.print(tree.getLast() + " ");
-		System.out.print(tree.getAvgRating() + " ");		
+		System.out.print("Student Number: " + tree.getStunum());
+		System.out.println();
+		System.out.print("Social Security Number: " + tree.getSocial());
+		System.out.println();
+		System.out.print("Last Name: " + tree.getLast());
+		System.out.println();
+		System.out.print("Idea Average Rating: " + tree.getAvgRating());	
+		System.out.println();	
+		System.out.println("Ideas: ");
+		System.out.println();
+		System.out.println(tree.getQueue().getIdeaQueue());
+		
+
 		
         if (tree.getLeft() != null){
         	System.out.println();
-	        System.out.print(tree.getLeft().getStunum() + " ");
-			System.out.print(tree.getLeft().getSocial() + " ");
-			System.out.print(tree.getLeft().getLast() + " ");
-			System.out.print(tree.getLeft().getAvgRating() + " ");
+	        System.out.print("Student Number: " + tree.getLeft().getStunum());
+	        System.out.println();
+			System.out.print("Social Security Number: " + tree.getLeft().getSocial());
+			System.out.println();
+			System.out.print("Last Name: " + tree.getLeft().getLast());
+			System.out.println();
+			System.out.print("Idea Average Rating: " + tree.getLeft().getAvgRating());
+			System.out.println();
 	    }	
         else if (tree.getRight() != null){
         	System.out.println();
-			System.out.print(tree.getRight().getStunum() + " ");
-			System.out.print(tree.getRight().getSocial() + " ");
-			System.out.print(tree.getRight().getLast() + " ");
-			System.out.print(tree.getRight().getAvgRating() + " ");	
+			System.out.print("Student Number: " + tree.getRight().getStunum());
+			System.out.println();
+			System.out.print("Social Security Number: " + tree.getRight().getSocial());
+			System.out.println();
+			System.out.print("Last Name: " + tree.getRight().getLast());
+			System.out.println();
+			System.out.print("Idea Average Rating: " + tree.getRight().getAvgRating());
+			System.out.println();	
 	    }	
         else {
-            System.out.println("There are no student records to show.");
+            System.out.println("There are no more student records to show.");
 	    }
 	}
     } 
 }
+
