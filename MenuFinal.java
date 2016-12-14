44a4fb1ff29020c36b9ecffcee50fd10836d50f7
Nathan Alexander Brown <nbrown2@conncoll.edu>
2016-12-13 20:58:30 -0500

    Update MenuFinal.java

--- MenuFinal.java
+++ MenuFinal.java
@@ -7,9 +7,9 @@ import java.io.*;
 public class MenuFinal implements java.io.Serializable {
 	public static void main(String[] args) {
 	BST bst = new BST();
-	BST2 bst2 = new BST2();
-	QueueA queue = new QueueA();
+	//QueueA queue = new QueueA();
 	Heap heap = new Heap();
+	QueueA queue = new QueueA();
 	
 System.out.println("Hello, Welcome to our Idea Database!");
 	while(1==1){	
@@ -25,9 +25,7 @@ System.out.println("Hello, Welcome to our Idea Database!");
 						FileInputStream fileIn = new FileInputStream("output.txt");
 						ObjectInputStream inn = new ObjectInputStream(fileIn);
 						bst = (BST) inn.readObject();
-						queue = (QueueA) inn.readObject();
 						heap = (Heap) inn.readObject();
-						bst2 = (BST2) inn.readObject();
 						inn.close();
 						fileIn.close();
 						} 
@@ -47,7 +45,7 @@ System.out.println("Hello, Welcome to our Idea Database!");
 				continue;
 			}
 // Note: for these^, BST, QueueA, etc are new objects we are assiging the stuff from the text fileout.close();
-		}
+			}
 
 	String input = "t";
 	while (!input.equals("E") || !input.equals("e")){	// while loop	
@@ -58,15 +56,54 @@ System.out.println("Hello, Welcome to our Idea Database!");
 		System.out.println("(B) Search for existing Student record");
 		System.out.println("(C) Print out list every student's name with their student #, SSN and avg score");
 		System.out.println("(D) Add new Idea using SSN search");
-		System.out.println("(E) Exit the program");
+		System.out.println("(E) Print best idea");
+		System.out.println("(F) Exit the program");
 		System.out.println();
 		System.out.println("What would you like to do? (Ex:'A'): "); // user prompt
 		input = in.next();
 		if (input.equals("A") || input.equals("a")) {	
-			bst.createStudentNode();
-			
+			//bst.createStudentNode();
+		//Scanner in = new Scanner(System.in);
+			System.out.println("Enter last name: ");
+			String lastName = in.next();
+			System.out.println("Enter login name: ");
+			String loginName = in.next();
+			String oldSSN;
+			int newSSN;
+			while(1==1){
+				System.out.println("Enter 9-Digit SSN: ");
+				oldSSN = in.next();
 
-			String input5 = "t";
+				newSSN = Integer.parseInt(oldSSN);
+				if (newSSN < 100000000 || newSSN > 999999999) {
+					System.out.println("That is an invalid SSN, please try again");
+					continue;
+				}
+				else{
+					//newSSN = newSSN % 10000;
+					break;
+				}
+			}
+			int studentNumber = 0;	
+			while(1==1){
+				System.out.println("Enter student number: ");
+				studentNumber = in.nextInt();
+				if (studentNumber < 1000 || studentNumber > 9999){
+					System.out.println("That is an invalid Student Number, please try again");
+					continue;}
+				else{
+					break;
+				}
+			}
+			StudentNode student = new StudentNode(lastName, loginName, newSSN, studentNumber);
+			in.nextLine();
+//HERE U NEED TO FIGURE OUT
+			bst.insert(student);
+			bst.insertSSN(student);
+
+		
+
+			/*String input5 = "g";
 			while (!input5.equals("B") || !input5.equals("b")){
 					System.out.println("Would you like to enter an Idea? (Ex. A) ");
 					System.out.println("(A) Yes");
@@ -81,17 +118,24 @@ System.out.println("Hello, Welcome to our Idea Database!");
 								continue;
 							}
 							else if (input2 > 99999999 && input2 < 1000000000){
-								queue.addIdeaStudent(input2);
+								StudentNode o = bst.searchRecordSSN(input2);
+								System.out.println(o.getLast());
+								
+								o.getQueue().setIdeaQueue(input2);
 								break;
 							}
 							else{
 								continue;
 							}
-						}
+					}
 					if (input5.equals("B") || input5.equals("b")){
 						break;
-				}
-			}
+					}
+			}*/
+			System.out.println();
+			System.out.println("Student Added!");
+			System.out.println("Redirecting back to the main menu...");
+			System.out.println();
 		}	
 		else if (input.equals("B") || input.equals("b")) {	
 			try {
@@ -108,7 +152,7 @@ System.out.println("Hello, Welcome to our Idea Database!");
 					input7 = in.next();
 					int input3 = Integer.parseInt(input7);
 					if (input3 > 99999999 && input3 < 1000000000){
-						StudentNode u = bst2.searchRecord(input3); 
+						StudentNode u = bst.searchRecordSSN(input3); 
 							if (u != null){
 							System.out.println("Student Found! Here is their information:");
 							System.out.println("Email: " + u.getEmail()); 
@@ -116,23 +160,27 @@ System.out.println("Hello, Welcome to our Idea Database!");
 						    System.out.println("Student Number: " + u.getStunum());
 						    System.out.println("Last Name: " + u.getLast()); 
 						    System.out.println("Average Idea Rating: " + u.getAvgRating());
+						    System.out.println();
+						    System.out.println("Redirecting back to main menu...");
 							}
 							else if (u == null) { 
-							System.out.println("Sorry that student record was not found!");
-							continue;
-						}	
+								System.out.println("Sorry that student record was not found!");
+								System.out.println("Redirecting...");
+								continue;
+							}	
 							System.out.println("Would you like to:");
 							System.out.println("(A) Delete the Student Record");
-							System.out.println("(B) Edit the Last Name or Login Name of the record");
-							System.out.println("Which would you like to do (Ex:'A'): ");
+							System.out.println("(B) Edit the Student Login credentials");
+							System.out.println("Which would you like to do?");
 							String input11 = in.next();
 						
-							try{
+							try {
 								if (input11.equals("A") || input11.equals("a")){
-									StudentNode a = bst2.searchRecord(input3);
+									StudentNode a = bst.searchRecordSSN(input3);
 									bst.delete(a);
-									bst2.delete(a);
+									bst.deleteSSN(a);
 									System.out.println("Student Record has been deleted");
+									System.out.println("Redirecting...");
 								}
 								else if(input11.equals("B") || input11.equals("b")){
 									System.out.println("Would you like to:");
@@ -143,25 +191,34 @@ System.out.println("Hello, Welcome to our Idea Database!");
 							
 								try{
 									if (input12.equals("A") || input12.equals("a")){
-										StudentNode a = bst2.searchRecord(input3);
+										StudentNode a = bst.searchRecordSSN(input3);
 										System.out.println("Enter new Last Name: ");
 										String input13 = in.next();
 										a.setLast(input13);
+										a.setLastSSN(input13);
+										System.out.println("Last name successfully updated!");
 									}
 									else if (input12.equals("B") || input12.equals("b")){
-										StudentNode a = bst2.searchRecord(input3);
+										StudentNode a = bst.searchRecordSSN(input3);
 										System.out.println("Enter new Login Name: ");
 										String input14 = in.next();
+										a.setEmailSSN(input14);
 										a.setEmail(input14);
+										System.out.println("Login name successfully updated!");
 									}
 									else if (input12.equals("C") || input12.equals("c")){
-										StudentNode a = bst2.searchRecord(input3);
+										StudentNode a = bst.searchRecordSSN(input3);
 										System.out.println("Enter new Last Name: ");
 										String input13 = in.next();
 										a.setLast(input13);
+										a.setLastSSN(input13);
+										System.out.println("Last name successfully updated!");
+										System.out.println();
 										System.out.println("Enter new Login Name: ");
 										String input14 = in.next();
 										a.setEmail(input14);
+										a.setEmailSSN(input14);
+										System.out.println("Login name successfully updated!");
 									}
 								}
 								catch(Exception e) {
@@ -181,6 +238,7 @@ System.out.println("Hello, Welcome to our Idea Database!");
 				catch(NumberFormatException e){
 					System.out.println();
 					System.out.println(e + "is an invalid SSN, please try again");
+					System.out.println();
 				}
 			}
 
@@ -215,18 +273,27 @@ System.out.println("Hello, Welcome to our Idea Database!");
 				String input9 = in.next();
 				int input10 = Integer.parseInt(input9);
 				if (input10 > 99999999 && input10 < 1000000000){
-					queue.addIdeaStudent(input10); 
+					StudentNode x = bst.searchRecordSSN(input10);
+					System.out.println(x.getLast());
+					x.getQueue().setIdeaQueue(input10);
 				}
 			}
 				catch(NumberFormatException e){
 					System.out.println();
 					System.out.println(e + "is an invalid SSN, please try again");
 				}
-		}
+			}
+			
 		else if (input.equals("E") || input.equals("e")) {
-			System.out.println("Thanks for using our program, have a lovely day");
+			System.out.println(heap.findMin());
+		
+		}
+		else if (input.equals("F") || input.equals("f")) {
+			System.out.println("Thanks for using our program, have a lovely day!!!");
+			System.out.println();
 			System.exit(0);
-		}	
+		}
+
 		else{
 			continue;
 		}
@@ -237,9 +304,8 @@ System.out.println("Hello, Welcome to our Idea Database!");
 			FileOutputStream fileOut = new FileOutputStream("output.txt");
 			ObjectOutputStream out = new ObjectOutputStream(fileOut);
 			out.writeObject(bst);
-			out.writeObject(queue);
 			out.writeObject(heap);
-			out.writeObject(bst2);
+
 			out.close();
 			fileOut.close();
 			System.out.println("Serialized object successfully in output.txt");
@@ -247,7 +313,7 @@ System.out.println("Hello, Welcome to our Idea Database!");
        		   catch(IOException i) {
 			  i.printStackTrace();
 			}
-	}
+	
 
 	System.exit(0);
 }
